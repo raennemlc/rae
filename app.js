94aeef358e393c1e5d565c699cbcd86059018bba
raennemlc <105567040+raennemlc@users.noreply.github.com>
2022-05-14 22:32:42 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,15 @@
+import selfcore from "selfcore";
+
+const client = new selfcore()
+const gateway = new selfcore.Gateway("USER_TOKEN")
+
+gateway.on("message", m =>{
+    if(m.channel_id === "CHANNEL_ID"){
+        let  content = m.content ? m.content : { embeds : [m.embeds[0]]};
+
+        client.sendWebhook(
+            "WEBHOOK_LINK",
+            content
+        );
+    }
+});
